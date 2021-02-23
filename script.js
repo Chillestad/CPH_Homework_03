a07bd9227ae981f58d8cd0a43a37da3bbb2912a4
chillestad <ColeHillestad18@gmail.com>
2021-02-22 22:58:44 -0600

    Application now generates a password of numbers, uppercase letters, lowercase letters and special characters of the users specified length.

--- script.js
+++ script.js
@@ -10,19 +10,35 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword(
   passwordLength, 
   includeSpecial,
-  iuncludeLowercase,
-  includeUppercase
+  includeLowercase,
+  includeUppercase,
+  includeNumber,
   ) {
 
    let characterOptions = [];
 
-   let lowercase = ["a", "b", "c", "d"];
-   let uppercase = ["A", "B", "C", "D"];
-   let special = ["!", "#", "@"];
+   let lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+   let uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+   let special = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+   let number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
    if (includeSpecial) {
      characterOptions = characterOptions.concat(special)
    }
+
+   if (includeUppercase) {
+      characterOptions = characterOptions.concat(uppercase)
+   }
+
+   if (includeLowercase) {
+    characterOptions = characterOptions.concat(lowercase)
+    }
+
+    if (includeNumber) {
+      characterOptions = characterOptions.concat(number)
+   }
+
+
 
     let res = ""
 
@@ -38,16 +54,22 @@ function generatePassword(
 
 // Write password to the #password input
 function writePassword() {
-   let passwordLength = 10;
+   let passwordLength = window.prompt("Enter Length");
+    // while (passwordLength <=8 || paswordLength <=128) {
+    //   window.alert("Password must be between 8-128 characters");
+    //   let passwordLength = window.prompt("Enter Length"));
+      
    let includeSpecial = true;
    let includeLowercase = true;
    let includeUppercase = false;
+   let includeNumber = true;
 
   var password = generatePassword(
       passwordLength, 
       includeSpecial,
       includeUppercase,
-      includeLowercase 
+      includeLowercase,
+      includeNumber
       );
   var passwordText = document.querySelector("#password");
 
